a326749d9a303494c1b9dc4623a27d00789384d3
watackeech <devkeech@gmail.com>
2023-05-29 14:50:26 +0900

    DAO interface の<T>を改善

--- src/DAOs/BookMasterDAO.java
+++ src/DAOs/BookMasterDAO.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class BookMasterDAO implements DAO {
+public class BookMasterDAO implements DAO<BookMaster> {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in, "SHIFT-JIS");
@@ -17,13 +17,6 @@ public class BookMasterDAO implements DAO {
         System.out.println("やりたい操作を教えてください");
         System.out.println("SHOW:1 INSERT:2 UPDATE:3 DELETE:4");
         int mode = scanner.nextInt();
-
-        // try {
-        // Class.forName("org.postgresql.Driver");
-        // System.out.println("ドライバーのロードに成功しました");
-        // } catch (ClassNotFoundException e) {
-        // throw new RuntimeException("ドライバーのロードに失敗しました", e);
-        // }
 
         ConnectionManager connectionManager = new ConnectionManager();
 
@@ -104,40 +97,24 @@ public class BookMasterDAO implements DAO {
         }
     }
 
-    // @Override
-    // public List<BookMaster> select(BookMaster bookMaster) {
-    // List<BookMaster> result;
-    // return result;
-    // throw new UnsupportedOperationException("Unimplemented method 'getAll'");
-    // }
-
     @Override
-    public void insert(Object entity) throws SQLException {
+    public void insert(BookMaster entity) throws SQLException {
         throw new UnsupportedOperationException("Unimplemented method 'insert'");
     }
 
     @Override
-    public void update(Object entity) throws SQLException {
+    public void update(BookMaster entity) throws SQLException {
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
     @Override
-    public void delete(Object entity) throws SQLException {
+    public void delete(BookMaster bookCondition) throws SQLException {
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
-    }
-
-    @Override
-    public List select(Object entity) throws SQLException {
-
-        List<BookMaster> result = null;
-        return result;
-        // throw new UnsupportedOperationException("Unimplemented method 'select'");
     }
 
     @Override
     public List<BookMaster> getAll() throws SQLException {
         ConnectionManager connectionManager = new ConnectionManager();
-
         try {
             Connection connection = connectionManager.getConnection();
             PreparedStatement preparedStatement = null;
@@ -194,15 +171,6 @@ public class BookMasterDAO implements DAO {
                     System.out.println("image: " + image);
                     System.out.println("------------------------");
                 }
-                // System.out.println(resultSet);
-                // while (resultSet.next()) {
-                // System.out.println("id:" + resultSet.getInt("id"));
-                // System.out.println("title:" + resultSet.getString("title"));
-                // System.out.println("author:" + resultSet.getString("author"));
-                // System.out.println("publication_year:" +
-                // resultSet.getInt("publication_year"));
-                // }
-
             } catch (SQLException e) {
                 throw new RuntimeException("EMPLOYEEテーブルのINSERTに失敗しました", e);
             } finally {
@@ -219,5 +187,87 @@ public class BookMasterDAO implements DAO {
             connectionManager.closeConnection();
         }
         throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+
     }
+
+    @Override
+    public List<BookMaster> select(BookMaster bookCondition) throws SQLException {
+        ConnectionManager connectionManager = new ConnectionManager();
+        try {
+            Connection connection = connectionManager.getConnection();
+            PreparedStatement preparedStatement = null;
+            try {
+                String sql = "SELECT * FROM book_master ORDER BY id WHERE 1 = 1 AND";
+
+                // BookMaster bookCondition = entity;
+                // SQLの作成(準備)
+                preparedStatement = connection.prepareStatement(sql);
+                // preparedStatement.setInt(1, masterId);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                List<BookMaster> result = new ArrayList<BookMaster>();
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String title = resultSet.getString("title");
+                    String author = resultSet.getString("author");
+                    int currentStock = resultSet.getInt("current_stock");
+                    int totalStock = resultSet.getInt("total_stock");
+                    int publicationYear = resultSet.getInt("publication_year");
+                    String category = resultSet.getString("category");
+                    String description = resultSet.getString("description");
+                    String image = resultSet.getString("image");
+
+                    // Create a new BookMaster object using the retrieved data
+                    BookMaster book = new BookMaster(id, title, author, currentStock, totalStock, publicationYear,
+                            category, description, image);
+
+                    // Add the BookMaster object to the result list
+                    result.add(book);
+                }
+
+                for (int i = 0; i < result.size(); i++) {
+                    BookMaster book = result.get(i);
+
+                    // Access the properties of the BookMaster object
+                    int id = book.getId();
+                    String title = book.getTitle();
+                    String author = book.getAuthor();
+                    int currentStock = book.getCurrentStock();
+                    int totalStock = book.getTotalStock();
+                    int publicationYear = book.getPublicationYear();
+                    String category = book.getCategory();
+                    String description = book.getDescription();
+                    String image = book.getImage();
+
+                    // Process the retrieved data as needed
+                    System.out.println("Book details at index " + i + ":");
+                    System.out.println("ID: " + id);
+                    System.out.println("Title: " + title);
+                    System.out.println("Author: " + author);
+                    System.out.println("Current Stock: " + currentStock);
+                    System.out.println("Total Stock: " + totalStock);
+                    System.out.println("Publication Year: " + publicationYear);
+                    System.out.println("Category: " + category);
+                    System.out.println("Description: " + description);
+                    System.out.println("image: " + image);
+                    System.out.println("------------------------");
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException("EMPLOYEEテーブルのINSERTに失敗しました", e);
+            } finally {
+                try {
+                    if (preparedStatement != null) {
+                        preparedStatement.close();
+                        System.out.println("ステートメントの解放に成功しました");
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException("ステートメントの解放に失敗しました", e);
+                }
+            }
+        } finally {
+            connectionManager.closeConnection();
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+
+    }
+
 }
