8a49c833217fa945a5188ee32d92f2da83b1a1c5
watackeech <devkeech@gmail.com>
2023-06-14 16:42:35 +0900

    書籍の一覧表示完成

--- WebContent/jsp/search-books.jsp
+++ WebContent/jsp/search-books.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +10,23 @@
 <title>Results from all books</title>
 </head>
 <body>
-
+	<table>
+		<thead>
+			<tr>
+				<th>タイトル</th>
+				<th>著者</th>
+				<th>発行年</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach var="book" items="${books}">
+				<tr>
+					<td>${book.title}</td>
+					<td>${book.author}</td>
+					<td>${book.publicationYear}</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
 </body>
 </html>
--- src/servlet/SearchBooksServlet.java
+++ src/servlet/SearchBooksServlet.java
@@ -1,12 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import DAOs.BookMasterDAO;
+import DAOs.ConnectionManager;
+import DTOs.BookMaster;
 
 /**
  * Servlet implementation class SearchBooksServlet
@@ -27,8 +34,21 @@ public class SearchBooksServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ConnectionManager connectionManager = new ConnectionManager();
+         // BookMasterDAOのインスタンスを作成
+
+        try {
+        	Connection con = connectionManager.getConnection();
+        	BookMasterDAO bookMasterDAO = new BookMasterDAO(con);
+            List<BookMaster> books = bookMasterDAO.getAll(); // すべての本の情報を取得
+
+            request.setAttribute("books", books); // 取得した本の情報をリクエスト属性にセット
+            request.getRequestDispatcher("/jsp/search-books.jsp").forward(request, response); // JSPにフォワードして表示
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // エラーハンドリング
+        }
 	}
 
 	/**
